b3e43ade30159121e61b0d25f3cded5e167d353a
bohu <shenbohu@gmail.com>
2021-06-17 16:09:52 +0800

    sa token

--- shenbohu/server/src/main/java/com/bohu/ServiceApplication.java
+++ shenbohu/server/src/main/java/com/bohu/ServiceApplication.java
@@ -4,7 +4,9 @@ package com.bohu;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -12,6 +14,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @EnableEurekaClient //开启Eureka客户端
 @MapperScan("com.bohu.dao")
+@EnableFeignClients
+@EnableDiscoveryClient
 @EnableTransactionManagement
 public class ServiceApplication {
 
